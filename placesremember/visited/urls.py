20cbfd51c4113fb8142b68cf5e6328a1c61e7ee0
G0tem <bindirin@yahoo.com>
2023-05-12 19:39:42 +0500

    Conclusion of memories

--- placesremember/visited/urls.py
+++ placesremember/visited/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),  #http://127.0.0.1:8000/
-    path('new/', newpost),
+    path('login/', login, name='login'),
+    path('new/', newpost, name='newpost'),
 ]
--- placesremember/visited/views.py
+++ placesremember/visited/views.py
@@ -2,13 +2,22 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 
-menu = ['Places Remember', 'Войти']
+menu = [{"title": 'Places Remember', 'url_name': 'home'},
+        {"title": 'Войти с помощью Google', 'url_name': 'login'},
+        ]
 def index(request):
     posts = Places.objects.all()
-    return render(request, 'visited/index.html', {"posts": posts, "menu": menu})
+    context = {
+        "posts": posts,
+        "menu": menu,
+    }
+    return render(request, 'visited/index.html', context=context)
 
 def newpost(request):
     return HttpResponse('Добавить новое место')
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
