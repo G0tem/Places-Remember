ba07328c727b9ce543fd19ac5931eb765cbe19f3
G0tem <bindirin@yahoo.com>
2023-05-16 18:23:38 +0500

    Fix form

--- placesremember/visited/forms.py
+++ placesremember/visited/forms.py
@@ -7,3 +7,7 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Places
         fields = ['user', 'latitude', 'longitude', 'name', 'comment']
+
+        widgets = {
+            'user': forms.TextInput(attrs={'style': 'display:none'}),
+        }
--- placesremember/visited/views.py
+++ placesremember/visited/views.py
@@ -33,7 +33,8 @@ class AddPost(LoginRequiredMixin, DataMixin, CreateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Добавление воспоминания')
+        c_def = self.get_user_context(title='Добавить воспоминания')
+        context['form'].initial['user'] = self.request.user
         return dict(list(context.items()) + list(c_def.items()))
 
 def login(request):
